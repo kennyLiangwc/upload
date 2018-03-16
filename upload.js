7c48a3818ee5a09fdfe71578d7d3a052adb1485e
Kenny <liangwc27@163.com>
2018-03-16 15:00:41 +0800

    全局暴露Upload方法

--- upload.js
+++ upload.js
@@ -1,111 +1,124 @@
 /*
 * @Author: KennyLiang
 * @Date:   2018-03-16 14:37:00
-* @Last Modified time: 2018-03-16 14:44:19
+* @Last Modified time: 2018-03-16 14:59:51
+* @param {String} file required
+* @param {String} url 服务端处理url required
+* @param {Function} progress 上传进度的回调函数
+* @param {Function} success 上传成功的回调函数
+* @param {Function} error 上传失败的回调函数
+* @param {Object} extraData 上传额外的参数
+* @param {Number} maxSize 允许上传最大的文件大小 default 50m
 */
+
 //默认允许上传最大文件大小 50m 
-let maxSize = 50 * 1024 * 1024;
 
-function buildFormData(task) {
-	let formData = new FormData();
-	for(let key in formData) {
-		if(task.hasOwnProperty(key)) {
-			formData.append(key,task[key])
+(function(win) {
+	let maxSize = 50 * 1024 * 1024;
+
+	function buildFormData(task) {
+		let formData = new FormData();
+		for(let key in formData) {
+			if(task.hasOwnProperty(key)) {
+				formData.append(key,task[key])
+			}
 		}
+		return formData
 	}
-	return formData
-}
-/*
- *上传器，绑定一个XMLHttpRequest对象
-*/
-function send(params) {
-	let task = params.task;
-	let url = params.url;
-	task.sendTime++;
-	let success = params.success || noop;
-	let progress = params.progress || noop;
-	let error = function(msg) {
-		(params.error || noop)(msg)
-	}
-	let formData = buildFormData(task);
-	let xhr = new XMLHttpRequest();
+	/*
+	 *上传器，绑定一个XMLHttpRequest对象
+	*/
+	function send(params) {
+		let task = params.task;
+		let url = params.url;
+		task.sendTime++;
+		let success = params.success || noop;
+		let progress = params.progress || noop;
+		let error = function(msg) {
+			(params.error || noop)(msg)
+		}
+		let formData = buildFormData(task);
+		let xhr = new XMLHttpRequest();
 
-	xhr.open("POST",url)
-	xhr.addEventListener("load", function() {
-		if(xhr.status == 200) {		//上传成功
-			try{
-				let json = JSON.parse(xhr.responseText);
-				if(json.ret !== 0) {
-					console.error(json.msg);
-					alert(json.msg);
+		xhr.open("POST",url)
+		xhr.addEventListener("load", function() {
+			if(xhr.status == 200) {		//上传成功
+				try{
+					let json = JSON.parse(xhr.responseText);
+					if(json.ret !== 0) {
+						console.error(json.msg);
+						alert(json.msg);
+						error('upload error')
+					}else {
+						success(json.data)
+					}
+				}catch(e) {
+					console.error('response error',e);
 					error('upload error')
-				}else {
-					success(json.data)
 				}
-			}catch(e) {
-				console.error('response error',e);
+			}else {
+				console.error('上传失败',xhr);
 				error('upload error')
 			}
-		}else {
-			console.error('上传失败',xhr);
+		});
+		xhr.onerror = function() {	//处理上传错误
+			console.error('xhr 出错了',arguments);
 			error('upload error')
+		};
+		xhr.upload.onprogress = function() {	//上传进度
+			let percentComplete = Math.floor(((e.loaded / e.total) || 0)) * 10000) / 100;
+			progress(percentComplete);
+			console.log(percentComplete)
 		}
-	});
-	xhr.onerror = function() {	//处理上传错误
-		console.error('xhr 出错了',arguments);
-		error('upload error')
+		xhr.send(formData)
 	};
-	xhr.upload.onprogress = function() {	//上传进度
-		let percentComplete = Math.floor(((e.loaded / e.total) || 0)) * 10000) / 100;
-		progress(percentComplete);
-		console.log(percentComplete)
-	}
-	xhr.send(formData)
-};
 
-function noop() {};
+	function noop() {};
 
-function Upload(params) {
-	let file = params.file;
-	let progress = params.progress || noop;
-	let success = params.success || noop;
-	let error = params.error || noop;
-	let extraData = params.extraData || {};
-	let url = params.url;
-	maxSize = params.maxSize || maxSize;
-	if(!file) {
-		error('文件为空');
-		return
-	}
-	if(file.size > maxSize) {
-		error('最大'+maxSize+',文件'+file.size);
-		return
-	}
-	//避免文件的重名导致服务端无法定位文件，需要给每个文件生产一个UUID
-	let uuid = [file.name,file.size,file.lastModified,+new Date()].join('_');
-	let task = {
-		data: file,
-		uuid: uuid
-	}
-	if(extraData) {
-		for(let key in extraData) {
-			if(extraData.hasOwnProperty(key)) {
-				task[key] = extraData[key]
+	function Upload(params) {
+		let file = params.file;
+		let progress = params.progress || noop;
+		let success = params.success || noop;
+		let error = params.error || noop;
+		let extraData = params.extraData || {};
+		let url = params.url;
+		maxSize = params.maxSize || maxSize;
+		if(!file) {
+			error('文件为空');
+			return
+		}
+		if(file.size > maxSize) {
+			error('最大'+maxSize+',文件'+file.size);
+			return
+		}
+		//避免文件的重名导致服务端无法定位文件，需要给每个文件生产一个UUID
+		let uuid = [file.name,file.size,file.lastModified,+new Date()].join('_');
+		let task = {
+			data: file,
+			uuid: uuid
+		}
+		if(extraData) {
+			for(let key in extraData) {
+				if(extraData.hasOwnProperty(key)) {
+					task[key] = extraData[key]
+				}
 			}
 		}
+		send({
+			url: url,
+			task: task,
+			success: function(data) {
+				if(data.url) {
+					progress(100);
+					success({
+						url: data.url
+					})
+				}
+			},
+			progress: progress,
+	        error: error
+		})
 	}
-	send({
-		url: url,
-		task: task,
-		success: function(data) {
-			if(data.url) {
-				progress(100);
-				success({
-					url: data.url
-				})
-			}
-		},
-		progress: progress,
-        error: error
-	})
-}
+	
+	win.Upload = Upload
+})(window)
